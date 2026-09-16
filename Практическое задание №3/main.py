c59f860ebf70aaec6d3ece437b89a90ed896f317
vlad5kd <60737365+vlad5kd@users.noreply.github.com>
2026-09-16 11:10:19 +0300

    Add files via upload

---
+++ Практическое задание №3/main.py
@@ -0,0 +1,19 @@
+a = 42
+b = 3.14159
+c = "Hello, Python!"
+d = True
+e = [1, 2, 3]
+f = (4, 5, 6)
+g = {"name": "Alice", "age": 30}
+h = {7, 8, 9}
+i = None
+
+print("a:", type(a))
+print("b:", type(b))
+print("c:", type(c))
+print("d:", type(d))
+print("e:", type(e))
+print("f:", type(f))
+print("g:", type(g))
+print("h:", type(h))
+print("i:", type(i))
