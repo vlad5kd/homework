d87fb0183f6c6782ef9da33481b001f32ff0ba2f
vlad5kd <60737365+vlad5kd@users.noreply.github.com>
2026-09-17 08:24:48 +0300

    Add files via upload

---
+++ Практическое задание №3/5. Работа с коллекциями.py
@@ -0,0 +1,6 @@
+shopping_list = ["Хлеб", "Молоко", "Мороженое" , "Сок"]
+shopping_list.append ("Сыр")
+print ("Список покупок:", shopping_list)
+unique_items = set(shopping_list)
+print("Множество покупок:", unique_items)
+# Список упорядоченный в том порядке, в котором его вводили, а множество перемешивает список.
